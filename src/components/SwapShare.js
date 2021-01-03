8c118d95cd299dd7c23ebe4526c1b053b6e2e162
Alec DiFederico <alecdifederico@gmail.com>
2021-01-03 13:44:27 -0500

    got all basic functionality working properly

--- src/components/SwapShare.js
+++ src/components/SwapShare.js
@@ -18,6 +18,7 @@ const SwapShare = ({web3, account, swapShareContract, DAIContract}) => {
     if (updateRequests) {
       getAddressBorrows()
       getAllRequests()
+      getAddressFulfilledLoans()
       setUpdateRequests(false)
     }
   }, [updateRequests])
@@ -33,6 +34,12 @@ const SwapShare = ({web3, account, swapShareContract, DAIContract}) => {
       setAllRequests(null)
     }
   }, [allRequests])
+
+  useEffect(() => {
+    if (fulfilledLoans && fulfilledLoans.length == 0) {
+      setFulfilledLoans(null)
+    }
+  }, [fulfilledLoans])
 
   function getAllRequests() {
     swapShareContract.methods
@@ -69,6 +76,7 @@ const SwapShare = ({web3, account, swapShareContract, DAIContract}) => {
         result.map(value => {
           if (value['valid']) {
             const expiration = new Date(parseInt(value['expiration']) * 1000)
+            const now = new Date()
 
             transactions.push({
               'index': value['index'],
@@ -78,11 +86,40 @@ const SwapShare = ({web3, account, swapShareContract, DAIContract}) => {
               'ethAmount': web3.utils.fromWei(value['ethAmount'], 'ether'),
               'ethPlusInterest': web3.utils.fromWei(value['ethPlusInterest'], 'ether'),
               'interestRate': value['interestRate'],
-              'state': value['state']
+              'state': value['state'],
+              'hasExpired': expiration < now
             })
           }
         })
         setBorrowTransactions(transactions)
+      })
+  }
+
+  function getAddressFulfilledLoans() {
+    swapShareContract.methods
+      .getAddressFulfilled(account)
+      .call()
+      .then(result => {
+        let transactions = [];
+        result.map(value => {
+          if (value['valid']) {
+            const expiration = new Date(parseInt(value['expiration']) * 1000)
+            const now = new Date()
+
+            transactions.push({
+              'index': value['index'],
+              'expirationDate': expiration.toDateString(),
+              'expirationTime': expiration.toLocaleTimeString(),
+              'daiAmount': web3.utils.fromWei(value['daiAmount'], 'ether'),
+              'ethAmount': web3.utils.fromWei(value['ethAmount'], 'ether'),
+              'ethPlusInterest': web3.utils.fromWei(value['ethPlusInterest'], 'ether'),
+              'interestRate': value['interestRate'],
+              'state': value['state'],
+              'hasExpired': expiration < now
+            })
+          }
+        })
+        setFulfilledLoans(transactions)
       })
   }
 
@@ -103,7 +140,31 @@ const SwapShare = ({web3, account, swapShareContract, DAIContract}) => {
         from: account,
         value: ethAmount
       })
-      .then(() => getAllRequests())
+      .then(() => {
+        getAllRequests()
+        getAddressFulfilledLoans()
+      })
+  }
+
+  const payBackLoan = (index, amount)  => () => {
+    const ethAmount = web3.utils.toWei(amount, 'ether').toString()
+
+    swapShareContract.methods
+      .repayLoan(index)
+      .send({
+        from: account,
+        value: ethAmount
+      })
+      .then(() => getAddressBorrows())
+  }
+
+  const claimCollateral = (index) => () => {
+    swapShareContract.methods
+      .claimDefaultedLoan(index)
+      .send({
+        from: account
+      })
+      .then(() => getAddressFulfilledLoans())
   }
 
   return (
@@ -140,6 +201,35 @@ const SwapShare = ({web3, account, swapShareContract, DAIContract}) => {
           daiContract={DAIContract} 
           setUpdateRequests={setUpdateRequests} 
         />
+        {fulfilledLoans && 
+          <div className="ml-4 mt-4">
+            <div className="mb-4">Loans You have Fulfilled</div>
+            {fulfilledLoans.map((value, index) => (
+              <Card className="mb-3" style={{color: '#282c34', minWidth: '20rem'}} key={index}>
+                  <Card.Header>{value.ethAmount} ETH requested</Card.Header>
+                  <Card.Body>
+                      <Card.Title>Posted collateral: {value.daiAmount} DAI</Card.Title>
+                      <Card.Text>Offered interest rate: {value.interestRate}%</Card.Text>
+                      <Card.Text>Total to be paid back: {value.ethPlusInterest} ETH</Card.Text>
+                      <Card.Text>Loan will be paid in full by:</Card.Text>
+                      <Card.Text>
+                          Date: {value.expirationDate}<br />
+                          Time: {value.expirationTime}
+                      </Card.Text>
+                      <div>
+                        <Button 
+                            variant={value.hasExpired ? 'success' : 'secondary'} 
+                            disabled={!value.hasExpired}
+                            onClick={claimCollateral(value.index)}
+                        >
+                          {value.hasExpired ? 'claim collateral' : 'active'}
+                        </Button>
+                      </div>
+                  </Card.Body>
+              </Card>
+            ))}
+          </div>
+        }
       </Body>
       <Body>
         {borrowTransactions &&
@@ -158,19 +248,30 @@ const SwapShare = ({web3, account, swapShareContract, DAIContract}) => {
                             Time: {value.expirationTime}
                         </Card.Text>
                         <div>
-                          <Button 
+                          {!value.hasExpired && 
+                            <Button 
                               variant={value.state == LOAN_STATE.requested ? 'danger' : 'success'} 
                               disabled={value.state == LOAN_STATE.fulfilled}
                               onClick={cancelBorrowRequest(value.index)}
-                          >
-                            {value.state == LOAN_STATE.requested ? 'cancel request' : 'active'}
-                          </Button>
-                          {/* <Button className="ml-2" variant="primary">Pay back loan</Button> */}
+                            >
+                              {value.state == LOAN_STATE.requested ? 'cancel request' : 'active'}
+                            </Button>
+                          }
+                          {value.state == LOAN_STATE.fulfilled &&
+                            <Button 
+                              variant={value.hasExpired ? "danger" : "primary"} 
+                              className="ml-2"
+                              disabled={value.hasExpired}
+                              onClick={payBackLoan(value.index, value.ethPlusInterest)}
+                            >
+                              {value.hasExpired ? 'Loan has Defaulted' : 'Pay back loan'}
+                            </Button>
+                          }
                         </div>
                     </Card.Body>
                 </Card>
             ))}
-            </div>
+          </div>
         }
       </Body>
     </div>
